f5e71fdd2b8d452c99e95c057e4e87bf6e9d20c4
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2020-04-25 23:33:30 +0200

    add App.js file to entry-server and entry-client files

--- .denide/entry-client.js
+++ .denide/entry-client.js
@@ -1,15 +1,78 @@
-import { createApp } from './App.js'
+import Vue from 'vue';
+import layout from '../layouts/default.vue';
+import { createRouter } from './router';
+import { createStore } from './store';
+import './mixin.js'
 import 'nprogress/nprogress.css'
 
-const { app, router } = createApp();
+const router = createRouter(page);
+const store = createStore(setCacheBag);
+const middlewares = []
 
-// register client plugins
-{{#plugins.client}}
+{{#middlewares}}
+middlewares.push( require('{{{ path }}}') )
+{{/middlewares}}
+
+const app = {
+  store,
+  router,
+  created () {
+    this.context = this.$options.context
+  },
+  render: (h) => h(layout)
+}
+
+let route = app.router.resolve( new URL( window.location ).pathname ).route
+
+app.context = {
+  store,
+  router,
+  route,
+  middlewares,
+  app,
+  req,
+  res
+}
+
+app.store.$router = app.router
+
+app.store.app = app
+
+function inject (key, value) {
+  key = '$' + key
+  Vue.use({ install (vm) { vm.prototype[key] = value } })
+  app[key] = value
+  app.store[key] = value
+}
+
+// register ssr plugins
+{{#plugins.ssr}}
   const plugin{{ index }} = require('{{{ src }}}')
   if ( typeof plugin{{ index }}.default === 'function' ) {
     plugin{{ index }}.default(app.context, inject)
   }
+{{/plugins.ssr}}
+
+// register client plugins
+{{#plugins.client}}
+const plugin{{ index }} = require('{{{ src }}}')
+if ( typeof plugin{{ index }}.default === 'function' ) {
+  plugin{{ index }}.default(app.context, inject)
+}
 {{/plugins.client}}
+
+function redirect (path) {
+  const url = new URL( window.location )
+  url.pathname = path
+  window.location = url.href
+}
+
+router.beforeEach((to, from, next) => {
+  middlewares.forEach( middleware => middleware({ ...app.context, redirect }) )
+  next()
+})
+
+const App = new Vue(app)
 
 router.onReady(() => {
   app.$mount('body > div');
--- .denide/entry-server.js
+++ .denide/entry-server.js
@@ -1,7 +1,65 @@
-import { createApp } from './App.js'
-export default function (...args) {
-  const { app, router, inject } = createApp(...args)
+import Vue from 'vue';
+import layout from '../layouts/default.vue';
+import { createRouter } from './router';
+import { createStore } from './store';
+import './mixin.js'
 
+export function createApp(page, req = {}, res = {}, setCacheBag, redirectServer) {
+  const router = createRouter(page);
+  const store = createStore(setCacheBag);
+  const middlewares = []
+
+  {{#middlewares}}
+  middlewares.push( require('{{{ path }}}') )
+  {{/middlewares}}
+
+  const app = {
+    store,
+    router,
+    created () {
+      this.context = this.$options.context
+    },
+    render: (h) => h(layout)
+  }
+
+  let route = {}
+
+  if ( typeof window !== 'object' ) {
+    route = app.router.resolve(req.path).route
+  } else {
+    route = app.router.resolve( new URL( window.location ).pathname ).route
+  }
+
+  app.context = {
+    store,
+    router,
+    route,
+    middlewares,
+    app,
+    req,
+    res
+  }
+
+  app.store.$router = app.router
+
+  app.store.app = app
+
+  function inject (key, value) {
+    key = '$' + key
+    app[key] = value
+    Vue.use({ install(vm) { vm.prototype[key] = value } })
+    app.store[key] = value
+  }
+
+  // register ssr plugins
+  {{#plugins.ssr}}
+    const plugin{{ index }} = require('{{{ src }}}')
+    if ( typeof plugin{{ index }}.default === 'function' ) {
+      plugin{{ index }}.default(app.context, inject)
+    }
+  {{/plugins.ssr}}
+
+  // register server side plugins
   {{#plugins.server}}
     const plugin{{ index }} = require('{{{ src }}}')
     if ( typeof plugin{{ index }}.default === 'function' ) {
@@ -9,5 +67,20 @@ export default function (...args) {
     }
   {{/plugins.server}}
 
-  return { app, router }
+  function redirect (path) {
+    if ( typeof window == 'object' ) {
+      const url = new URL( window.location )
+      url.pathname = path
+      window.location = url.href
+    } else {
+      redirectServer(path)
+    }
+  }
+
+  router.beforeEach((to, from, next) => {
+    middlewares.forEach( middleware => middleware({ ...app.context, redirect }) )
+    next()
+  })
+
+  return { app : new Vue(app), router };
 }
